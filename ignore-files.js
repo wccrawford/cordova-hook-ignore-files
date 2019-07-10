0532055508916c6f6494171f9afafa5f4d46ceb1
William Crawford <wccrawford@gmail.com>
2019-07-10 07:03:36 -0400

    Fixed async deletion.

--- ignore-files.js
+++ ignore-files.js
@@ -3,10 +3,29 @@ module.exports = function(context) {
   var fs = require('fs'),
   del = require('del');
   path = require('path'),
-  deferral = require('q').defer(),
+  Q = require('q'),
+  deferral = Q.defer(),
   xml2js = require('xml2js'),
   parser = new xml2js.Parser(),
   glob = require("glob");
+
+  function delFiles(ignoreFiles, config) {
+      var deferral = Q.defer(),
+          fileDeferrals = [];
+
+      glob(ignoreFiles, {}, function (er, files) {
+          for (var i = 0, l = files.length; i < l; ++i) {
+              fileDeferrals.push(del(files[i]));
+              console.log('ignore-files.js: File `' + files[i] + '` ignored.');
+          }
+
+          deferral.resolve();
+      })
+
+      return deferral.promise.then(function() {
+          return Q.all(fileDeferrals);
+      });
+  }
 
   fs.readFile('config.xml', 'utf8', function (err, xml) {
     if (err) {
@@ -23,18 +42,19 @@ module.exports = function(context) {
       }
       var ignoreFiles = config.widget['ignore-files'];
 
+      var globDeferrals = [];
       for (var i = 0, l = ignoreFiles.length; i < l; ++i) {
         if (ignoreFiles[i].$ === undefined || ignoreFiles[i].$.ignore === undefined) {
           continue;
         }
-        glob(ignoreFiles[i].$.ignore, {}, function (er, files) {
-          for (var i = 0, l = files.length; i < l; ++i) {
-            del.sync(files[i]);
-            console.log('ignore-files.js: File `' + files[i] + '` ignored.');
-          }
-        })
+
+        globDeferrals.push(
+          delFiles(ignoreFiles[i].$.ignore, {})
+        );
       }
-      deferral.resolve();
+      Q.all(globDeferrals).done(function() {
+          deferral.resolve();
+      });
     });
   });
 
