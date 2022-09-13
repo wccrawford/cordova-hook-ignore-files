fecd9eedc13483f9867bc56cbd6841e5d5fa41bc
William Crawford <william.crawford@bluetoad.com>
2022-09-13 13:03:19 -0400

    Used real promises and fs rm

--- ignore-files.js
+++ ignore-files.js
@@ -3,70 +3,70 @@ var q = require('q');
 var fs = require('fs');
 var glob = require('glob');
 var xml2js = require('xml2js');
-var del = require('del');
-// import {deleteAsync} from 'del';
+var fsProm = require('fs/promises');
 
-module.exports = function(context) {
-  var deferral = q.defer();
-  var parser = new xml2js.Parser();
+module.exports = async function(context) {
+    return new q.Promise((resolve, reject) => {
+        var parser = new xml2js.Parser();
 
-  function delFiles(ignoreFiles, config) {
-      var deferral = q.defer(),
-          fileDeferrals = [];
+        async function delFiles(ignoreFiles, config) {
+            return new q.Promise((resolve, reject) => {
+                var fileDeferrals = [];
 
-      glob(ignoreFiles.$.ignore, {}, function (er, files) {
-          if (!files.length) {
-              console.log('ignore-files.js: Nothing to ignore with pattern `' + ignoreFiles.$.ignore + '`.');
+                glob(ignoreFiles.$.ignore, {}, function (er, files) {
+                    if (!files.length) {
+                        console.log('ignore-files.js: Nothing to ignore with pattern `' + ignoreFiles.$.ignore + '`.');
 
-              return;
-          }
-          console.log('ignore-files.js: Ignoring with pattern `' + ignoreFiles.$.ignore + '`.');
+                        return;
+                    }
+                    console.log('ignore-files.js: Ignoring with pattern `' + ignoreFiles.$.ignore + '`.');
 
-          for (var i = 0, l = files.length; i < l; ++i) {
-              fileDeferrals.push(del(files[i]));
-              console.log('ignore-files.js: File `' + files[i] + '` ignored.');
-          }
+                    for (var i = 0, l = files.length; i < l; ++i) {
+                        fileDeferrals.push(
+                            fsProm.rm(files[i], {force:true, recursive: true}).catch((exc) => {console.log('Exception', exc)})
+                        );
+                        // console.log('ignore-files.js: File `' + files[i] + '` ignored.');
+                    }
 
-          deferral.resolve();
-      })
+                });
 
-      return deferral.promise.then(function() {
-          return q.all(fileDeferrals);
-      });
-  }
-
-  fs.readFile('config.xml', 'utf8', function (err, xml) {
-    if (err) {
-      deferral.reject('Unable to load config.xml');
-
-      return;
-    }
-    parser.parseString(xml, function (err, config) {
-      if (err) {
-        deferral.reject('Unable to parse config.xml');
-
-        return;
-      }
-      if (config.widget === undefined || config.widget['ignore-files'] === undefined) {
-        return;
-      }
-      var ignoreFiles = config.widget['ignore-files'];
-
-      var globDeferrals = [];
-      for (var i = 0, l = ignoreFiles.length; i < l; ++i) {
-        if (ignoreFiles[i].$ === undefined || ignoreFiles[i].$.ignore === undefined) {
-          continue;
+                q.all(fileDeferrals).done(() => {
+                    resolve();
+                });
+            });
         }
 
-        globDeferrals.push(
-          delFiles(ignoreFiles[i], {})
-        );
-      }
-      q.all(globDeferrals).done(function() {
-          deferral.resolve();
-      });
-    });
-  });
+        fs.readFile('config.xml', 'utf8', function (err, xml) {
+            if (err) {
+                reject('Unable to load config.xml');
 
-  return deferral.promise;
+                return;
+            }
+            parser.parseString(xml, function (err, config) {
+                if (err) {
+                    reject('Unable to parse config.xml');
+
+                    return;
+                }
+                if (config.widget === undefined || config.widget['ignore-files'] === undefined) {
+                    return;
+                }
+                var ignoreFiles = config.widget['ignore-files'];
+
+                var globDeferrals = [];
+                for (var i = 0, l = ignoreFiles.length; i < l; ++i) {
+                    if (ignoreFiles[i].$ === undefined || ignoreFiles[i].$.ignore === undefined) {
+                        continue;
+                    }
+
+                    globDeferrals.push(
+                        delFiles(ignoreFiles[i], {})
+                    );
+                }
+                q.all(globDeferrals).done(function () {
+                    resolve();
+                });
+            });
+        });
+    });
 };
